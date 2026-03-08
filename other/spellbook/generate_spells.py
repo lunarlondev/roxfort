12a54f7b145766b0a946d276c77a924a1778024f
Lunarlona <notcaringenough@gmail.com>
2026-03-08 12:35:56 +0100

    up

---
+++ other/spellbook/generate_spells.py
@@ -0,0 +1,72 @@
+import requests
+import json
+
+API = "https://harrypotter.fandom.com/api.php"
+
+categories = [
+    "Category:Spells",
+    "Category:Charms",
+    "Category:Curses",
+    "Category:Hexes",
+    "Category:Jinxes",
+    "Category:Healing_spells",
+    "Category:Transfiguration_spells",
+    "Category:Dark_Arts"
+]
+
+spells = {}
+
+print("Varázslatok lekérése...")
+
+for cat in categories:
+
+    continue_token = None
+
+    while True:
+
+        params = {
+            "action": "query",
+            "list": "categorymembers",
+            "cmtitle": cat,
+            "cmlimit": "500",
+            "format": "json"
+        }
+
+        if continue_token:
+            params["cmcontinue"] = continue_token
+
+        r = requests.get(API, params=params)
+        r.raise_for_status()
+
+        data = r.json()
+
+        for item in data["query"]["categorymembers"]:
+
+            name = item["title"]
+
+            spells[name] = {
+                "name": name,
+                "hu": "-",
+                "year": 8,
+                "category": "bubajok",
+                "description": "",
+                "effects": [],
+                "dark": False,
+                "wiki": "https://harrypotter.fandom.com/wiki/" + name.replace(" ", "_")
+            }
+
+        if "continue" in data:
+            continue_token = data["continue"]["cmcontinue"]
+        else:
+            break
+
+
+spell_list = list(spells.values())
+
+print("Talált varázslatok:", len(spell_list))
+
+with open("spells.json", "w", encoding="utf-8") as f:
+    json.dump(spell_list, f, indent=2, ensure_ascii=False)
+
+print("spells.json létrehozva!")
+input("Enter a kilépéshez...")
