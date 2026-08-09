348fee769a37afd94d3548993370ee122d869dd5
Lunarlona <notcaringenough@gmail.com>
2026-08-09 14:13:57 +0200

    .

---
+++ Barbon/pw/test.js
@@ -0,0 +1,7 @@
+const testBox = document.getElementById("js-test");
+
+if (testBox) {
+    testBox.innerHTML = "✅ A külső JavaScript sikeresen betöltődött!";
+    testBox.style.background = "#26734d";
+    testBox.style.fontSize = "20px";
+}
